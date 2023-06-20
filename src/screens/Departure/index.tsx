0917977e570d6d893dfc2a82db62cee8bc52ffbb
Gustavoctt <gustavocarrertartare@gmail.com>
2023-06-20 18:44:03 -0300

    feat: create inputs lisense and text area

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,10 +1,53 @@
+import { useRef } from "react";
+import { Container, Content } from "./styles";
 import { Header } from "../../components/Header";
-import { Container } from "./styles";
+import { Button } from "../../components/Button";
+import { TextAreaInput } from "../../components/TextAreaInput";
+import { LicensePlateInput } from "../../components/LisensePlateInput";
+import { ScrollView, KeyboardAvoidingView, Platform } from "react-native";
+
+const keyboardAvoidingViewBehavior =
+  Platform.OS === "android" ? "height" : "position";
 
 export function Departure() {
+  const descriptionRef = useRef(null);
+
+  function handleDepartureRegister() {
+    console.log("OK");
+  }
+
   return (
     <Container>
       <Header title="Saída" />
+
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={keyboardAvoidingViewBehavior}
+      >
+        <ScrollView>
+          <Content>
+            <LicensePlateInput
+              label="Placa do veículo"
+              placeholder="BRA1234"
+              onSubmitEditing={() => {
+                descriptionRef.current?.focus();
+              }}
+              returnKeyType="next"
+            />
+
+            <TextAreaInput
+              ref={descriptionRef}
+              label="Finalidade"
+              placeholder="Vou utilizar o veículo para..."
+              onSubmitEditing={handleDepartureRegister}
+              returnKeyType="send"
+              blurOnSubmit
+            />
+
+            <Button title="Registrar saída" onPress={handleDepartureRegister} />
+          </Content>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
